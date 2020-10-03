5e10bbaeae0ff491574ecee3369cd043c6651202
VisahlBhanderi31 <VisahlBhanderi07@gmail.com>
2020-10-03 16:15:43 +0530

    Added data in database

--- WebApiWithASP.Net/Migrations/Configuration.cs
+++ WebApiWithASP.Net/Migrations/Configuration.cs
@@ -16,7 +16,11 @@ namespace WebApiWithASP.Net.Migrations
         {
             context.Employees.AddOrUpdate(x => x.Id, 
                 new Models.Employee() {Id  =1,Name = "Vishal Bhanderi"},
-                new Models.Employee() {Id = 2,Name = "JAy Ramani" });
+                new Models.Employee() {Id = 2,Name = "JAy Ramani" },
+                new Models.Employee() { Id = 3, Name = "Aarti Bhanderi" },
+                new Models.Employee() { Id = 4, Name = "Bhautik Dholariya" },
+                new Models.Employee() { Id = 5, Name = "Akash B Patel" }
+                );
 
             context.Offices.AddOrUpdate(x => x.Id,
                 new Models.Office()
@@ -24,7 +28,14 @@ namespace WebApiWithASP.Net.Migrations
                     Id = 101,
                     Location = "Ahmedabad",
                     EmployeeId = 1
-                });
+                },
+                new Models.Office()
+                {
+                    Id = 102,
+                    Location = "Surat",
+                    EmployeeId = 2
+                }
+                );
         }
     }
 }
